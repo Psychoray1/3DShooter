710d086668cb08f9d171942e075fa42afbc864a5
@Psychoray <n.sagynalieva1406@gmail.com>
2024-12-18 03:18:52 +0100

    Changes were made

---
+++ Assets/Scripts/MouseMovement.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+   public float mouseSensitivity = 500f;
+
+   float xRotation = 0f;
+   float yRotation = 0f;
+
+   public float topClamp = -90f;
+   public float bottomClamp = 90f;
+
+    void Start()
+    {
+        //Locking the cursor to the middle of the screen and making it invisible
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    
+    void Update()
+    {
+        //Getting the mouse inputs
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //Rotation around the x axis (Look up and down)
+        xRotation -= mouseY;
+
+        //Clamp the Rotation 
+        xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
+
+        //Rotation around the x axis (Look left and right)
+        yRotation -= mouseX;   
+
+        //Apply rotations to our transform
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+
+
+
+    }
+}
